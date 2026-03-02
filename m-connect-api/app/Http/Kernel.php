6ad6687c42f159734e84e6af097bd173472345a8
ReganLema <reganlema44@gmail.com>
2026-03-02 10:01:26 +0300

    Remove artisan serve from Railway config

--- m-connect-api/app/Http/Kernel.php
+++ m-connect-api/app/Http/Kernel.php
@@ -2,3 +2,8 @@ protected $routeMiddleware = [
     // ... existing middleware
     'role' => \App\Http\Middleware\CheckFarmerRole::class,
 ];
+
+protected $commands = [
+    \App\Console\Commands\CacheClearPatternCommand::class,
+    \App\Console\Commands\CacheStatsCommand::class,
+];
